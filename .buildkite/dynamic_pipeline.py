0cd63fdfe0c2781955e9f287e096bb8e4e807b68
Gavrie Philipson <gavrie@philipson.co.il>
2025-07-10 15:59:34 +0300

    wip

--- .buildkite/dynamic_pipeline.py
+++ .buildkite/dynamic_pipeline.py
@@ -1,7 +1,15 @@
 from buildkite_sdk import CommandStep, Pipeline
 
 pipeline = Pipeline()
-pipeline.add_step(CommandStep(commands="echo 'Hello, world!'"))
+pipeline.add_step(
+    CommandStep(
+        commands=[
+            "echo 'Hello, world!'",
+            "false",
+        ],
+    )
+)
+# pipeline.add_step(CommandStep(commands="false"))
 
 # JSON output
 # print(pipeline.to_json())
